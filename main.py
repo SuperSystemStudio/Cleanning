7053da6fc0481478d8a6cd53ff28b6b35cf07dd8
Mryan2005 <40586009+Mryan2005@users.noreply.github.com>
2021-07-03 22:03:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import configparser
+import os
+import sys
 config.read("path.ini")
 config=configparser.ConfigParser()
-if 
-config.add_section("path")
-config.set("School","IP","192.168.1.120")
+config = open("./path.ini"，"r")
+if config.read() == "":
+  config.add_section("path")
+  for root in os.walk("C:\Users\Lenovo\AppData\Local\kingsoft\WPS Cloud Files\userdata\qing\filecache"):
+    if root != ".3172735" or root != "configbackup":
+      config.set("path","WPS","C:\Users\Lenovo\AppData\Local\kingsoft\WPS Cloud Files\userdata\qing\filecache"+root)
+if sys.argv[1] == "clean":
+  for a in config.sections("path")
+    for i in config.get("path", a)
+  os.removedirs(i)
